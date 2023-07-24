038ce48e0ee9e8f9aee96eace52c8a7f00bc5211
José Andrade <jandrad@gmail.com>
2023-07-24 14:34:21 +0200

    Use crypto.com for price feed

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { CronJob } from "cron";
 import { get_ticker as getBitFinexTicker } from "./plugins/bitfinex";
 import { get_ticker as getCryptoPricesTicker } from "./plugins/cryptoprices";
+import { get_ticker as getCryptoComTicker } from "./plugins/cryptocom";
 import { Quote, write } from "./plugins/delphioracle";
 import { transact } from "./src/utils";
 import { api, CRON_INTERVAL, ACCOUNT, AUTHORIZATION } from "./src/config";
@@ -10,17 +11,17 @@ new CronJob(
   CRON_INTERVAL,
   async () => {
     const [usd, btc, eth, eosUsd] = await Promise.all([
-      getCryptoPricesTicker("WAXP"),
-      getBitFinexTicker("tBTCUSD"),
-      getBitFinexTicker("tETHUSD"),
-      getBitFinexTicker("tEOSUSD"),
+      getCryptoComTicker("WAXP_USD"),
+      getCryptoComTicker("BTC_USD"),
+      getCryptoComTicker("ETH_USD"),
+      getCryptoComTicker("EOS_USD"),
     ]);
 
-    const btcRate = Number(usd.lastPrice) / Number(btc.lastPrice);
-    const ethRate = Number(usd.lastPrice) / Number(eth.lastPrice);
-    const eosRate = Number(usd.lastPrice) / Number(eosUsd.lastPrice);
+    const btcRate = Number(usd.a) / Number(btc.a);
+    const ethRate = Number(usd.a) / Number(eth.a);
+    const eosRate = Number(usd.a) / Number(eosUsd.a);
     const quotes: Quote[] = [
-      { pair: "waxpusd", value: to_uint(usd.lastPrice, 4) },
+      { pair: "waxpusd", value: to_uint(usd.a, 4) },
       { pair: "waxpbtc", value: to_uint(btcRate, 8) },
       { pair: "waxpeth", value: to_uint(ethRate, 8) },
       { pair: "waxpeos", value: to_uint(eosRate, 6) },
